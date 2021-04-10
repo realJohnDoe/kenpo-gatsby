65a79a9a182b01cf88ac5056e1d5648796053d08
phartenfeller <pharti@hotmail.de>
2021-04-10 20:58:03 +0200

    fix dockerfile import

--- gatsby-philipps-foam-theme/src/components/mdx-components/CodeMirror6.jsx
+++ gatsby-philipps-foam-theme/src/components/mdx-components/CodeMirror6.jsx
@@ -1,45 +1,45 @@
 // import { basicSetup } from "@codemirror/basic-setup";
-import { lineNumbers } from "@codemirror/gutter";
-import { css } from "@codemirror/lang-css";
-import { html } from "@codemirror/lang-html";
-import { javascript } from "@codemirror/lang-javascript";
-import { json } from "@codemirror/lang-json";
-import { dockerfile } from "@codemirror/legacy-modes/mode/dockerfile";
-import { shell } from "@codemirror/legacy-modes/mode/shell";
-import { plSQL } from "@codemirror/legacy-modes/mode/sql";
-import { yaml } from "@codemirror/legacy-modes/mode/yaml";
-import { EditorState } from "@codemirror/state";
-import { StreamLanguage } from "@codemirror/stream-parser";
-import { oneDark } from "@codemirror/theme-one-dark";
-import { EditorView } from "@codemirror/view";
-import React, { useEffect, useRef } from "react";
+import { lineNumbers } from '@codemirror/gutter';
+import { css } from '@codemirror/lang-css';
+import { html } from '@codemirror/lang-html';
+import { javascript } from '@codemirror/lang-javascript';
+import { json } from '@codemirror/lang-json';
+import { dockerFile } from '@codemirror/legacy-modes/mode/dockerfile';
+import { shell } from '@codemirror/legacy-modes/mode/shell';
+import { plSQL } from '@codemirror/legacy-modes/mode/sql';
+import { yaml } from '@codemirror/legacy-modes/mode/yaml';
+import { EditorState } from '@codemirror/state';
+import { StreamLanguage } from '@codemirror/stream-parser';
+import { oneDark } from '@codemirror/theme-one-dark';
+import { EditorView } from '@codemirror/view';
+import React, { useEffect, useRef } from 'react';
 
 const getLanguage = (className) => {
   // https://github.com/codemirror/legacy-modes
-  const lang = className ? className.replace(`language-`, "") : null;
+  const lang = className ? className.replace(`language-`, '') : null;
   switch (lang) {
-    case "javascript":
-    case "js":
+    case 'javascript':
+    case 'js':
       return javascript();
-    case "json":
+    case 'json':
       return json();
-    case "css":
+    case 'css':
       return css();
-    case "html":
+    case 'html':
       return html();
-    case "dockerfile":
-      return StreamLanguage.define(dockerfile);
-    case "sh":
-    case "bash":
-    case "service":
-    case "shell":
+    case 'dockerfile':
+      return StreamLanguage.define(dockerFile);
+    case 'sh':
+    case 'bash':
+    case 'service':
+    case 'shell':
       return StreamLanguage.define(shell);
-    case "sql":
-    case "plsql":
-    case "pl/sql":
+    case 'sql':
+    case 'plsql':
+    case 'pl/sql':
       return StreamLanguage.define(plSQL);
-    case "yaml":
-    case "yml":
+    case 'yaml':
+    case 'yml':
       return StreamLanguage.define(yaml);
     default:
       if (lang) {
@@ -61,8 +61,8 @@ const CodeMirror = ({ children, className }) => {
   useEffect(() => {
     if (editorRef.current === undefined) {
       const FontSizeTheme = EditorView.theme({
-        "&": {
-          fontSize: "14px",
+        '&': {
+          fontSize: '14px',
         },
       });
 
