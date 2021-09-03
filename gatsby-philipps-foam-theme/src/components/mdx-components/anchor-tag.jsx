3eaaa30870e04b504bc7e3922a455020bf980af2
phartenfeller <pharti@hotmail.de>
2021-09-03 13:03:23 +0200

    remove foam link from anchor-tag

--- gatsby-philipps-foam-theme/src/components/mdx-components/anchor-tag.jsx
+++ gatsby-philipps-foam-theme/src/components/mdx-components/anchor-tag.jsx
@@ -12,18 +12,6 @@ const custBasename = (filePath) => {
   return split[split.length - 1];
 };
 
-function isLinkToExcludedPage({ externalLink, content }) {
-  // check if has square brackets
-  const brackets = /\[\[.*\]\]/i.test(content);
-
-  // strange but with that we can be sure that it points to an excluded page
-  if (externalLink === false && brackets === true) {
-    return true;
-  }
-
-  return false;
-}
-
 export const AnchorTag = ({
   title,
   href,
@@ -32,113 +20,26 @@ export const AnchorTag = ({
   withoutPopup,
   ...restProps
 }) => {
-  // same as in gatsby-transformer-markdown-references/src/compute-inbounds.ts#getRef
-  const ref = references.find(
-    (x) =>
-      withPrefix(x.slug) === withPrefix(href) ||
-      x.title === title ||
-      (x.aliases || []).some((alias) => alias === title) ||
-      custBasename(x.slug) === title
-  );
-
-  let content;
-  let popupContent;
   let child;
 
-  if (ref) {
-    const nestedComponents = {
-      a(props) {
-        return <AnchorTag {...props} references={references} withoutLink />;
-      },
-      p(props) {
-        return <span {...props} />;
-      },
-    };
-    content =
-      ref.title === ref.displayTitle ? (
-        restProps.children
-      ) : (
-        <MDXProvider components={nestedComponents}>
-          <MDXRenderer>{ref.mdx}</MDXRenderer>
-        </MDXProvider>
-      );
-    popupContent = (
-      <div id={ref.id} className="tw-popover">
-        {ref.title === ref.displayTitle ? (
-          <>
-            <MDXProvider components={nestedComponents}>
-              <MDXRenderer>{ref.mdx}</MDXRenderer>
-            </MDXProvider>
-          </>
-        ) : (
-          <>
-            <h5>{ref.displayTitle}</h5>
-            <ul>
-              <li>
-                <MDXProvider components={nestedComponents}>
-                  <MDXRenderer>{ref.mdx}</MDXRenderer>
-                </MDXProvider>
-              </li>
-            </ul>
-          </>
-        )}
-      </div>
-    );
-    const withoutBrackes = content.replace(/^\[\[/, '').replace(/\]\]$/, '');
+  const content = restProps.children;
+  const externalLink = /^(http(s?)):\/\//i.test(href);
+
+  if (withoutLink) {
+    return <span>{content}</span>;
+  }
+
+  if (withoutPopup) {
+    return child;
+  }
+
+  const popupContent = <div className="tw-popover">{href}</div>;
+
+  if (externalLink) {
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
     child = (
-      <>
-        <span className="text-skin-secondary">[[</span>
-        <LinkToStacked {...restProps} to={ref.slug} title={title}>
-          {withoutBrackes}
-        </LinkToStacked>
-        <span className="text-skin-secondary">]]</span>
-      </>
-    );
-  } else {
-    content = restProps.children;
-    const externalLink = /^(http(s?)):\/\//i.test(href);
-
-    if (isLinkToExcludedPage({ externalLink, withoutLink, content })) {
-      return (
-        <span className="px-1 rounded bg-skin-secondary text-skin-secondary cursor-not-allowed tracking-wide">
-          excluded page
-        </span>
-      );
-    }
-
-    if (withoutLink) {
-      return <span>{content}</span>;
-    }
-
-    if (withoutPopup) {
-      return child;
-    }
-
-    popupContent = <div className="tw-popover">{href}</div>;
-
-    if (externalLink) {
-      // eslint-disable-next-line jsx-a11y/anchor-has-content
-      child = (
-        <span className="inline-flex items-center">
-          {/* eslint-disable-next-line jsx-a11y/anchor-has-content,react/jsx-no-target-blank */}
-          <a
-            {...restProps}
-            href={
-              !href || (href.indexOf && href.indexOf('#') === 0)
-                ? href
-                : withPrefix(href)
-            }
-            title={title}
-            target="_blank"
-            rel="noopener noreferrer"
-          />
-          <ExternalLinkIcon className="inline-block text-skin-secondary w-4 h-4" />
-        </span>
-      );
-    } else {
-      // eslint-disable-next-line jsx-a11y/anchor-has-content
-      child = (
-        // eslint-disable-next-line jsx-a11y/anchor-has-content
+      <span className="inline-flex items-center">
+        {/* eslint-disable-next-line jsx-a11y/anchor-has-content,react/jsx-no-target-blank */}
         <a
           {...restProps}
           href={
@@ -147,9 +48,26 @@ export const AnchorTag = ({
               : withPrefix(href)
           }
           title={title}
+          target="_blank"
+          rel="noopener noreferrer"
         />
-      );
-    }
+        <ExternalLinkIcon className="inline-block text-skin-secondary w-4 h-4" />
+      </span>
+    );
+  } else {
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    child = (
+      // eslint-disable-next-line jsx-a11y/anchor-has-content
+      <a
+        {...restProps}
+        href={
+          !href || (href.indexOf && href.indexOf('#') === 0)
+            ? href
+            : withPrefix(href)
+        }
+        title={title}
+      />
+    );
   }
 
   return (
